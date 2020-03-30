970411f5b1542dffd9f212bf152672f5d5fb4c7b
Aviral Tandon <61861921+aviraltandon21@users.noreply.github.com>
2020-03-30 21:39:14 +0530

    Add files via upload

---
+++ colorgame.js
@@ -0,0 +1,81 @@
+ var colors = generaterandomcolors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedcolor = pickcolor();
+var colordisplay = document.getElementById("colordisplay");
+var messagedisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetbutton = document.querySelector("#reset");
+var sound = new Howl({
+  src: ['Music/bubbles.mp3']
+});
+var sound2 = new Howl({
+  src: ['Music/prism-2.mp3']
+});
+
+colordisplay.textContent = pickedcolor;
+resetbutton.addEventListener("click", function(){
+	colors = generaterandomcolors(6);
+	pickedcolor = pickcolor();
+	colordisplay.textContent = pickedcolor;
+	for(var i=0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+})
+
+for(var i=0; i<squares.length; i++){
+squares[i].style.backgroundColor = colors[i];
+
+squares[i].addEventListener("click",function(){
+
+	
+	var clickedcolor = this.style.backgroundColor;
+	if(clickedcolor === pickedcolor)
+	{
+        sound2.play();
+		messagedisplay.textContent = "correct";
+		resetbutton.textContent = "Play Again?";
+		changecolors(clickedcolor);
+		h1.style.backgroundColor = clickedcolor;
+		
+	}
+	else {
+		this.style.backgroundColor = "#232323";
+		sound.play();
+		messagedisplay.textContent = "try again";
+	}
+});
+} 
+
+function changecolors(color){
+
+for(var i=0; i<squares.length; i++)
+{
+	squares[i].style.backgroundColor = color;
+}
+}
+
+function pickcolor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generaterandomcolors(num) {
+var arr = []
+for(var i=0; i<num; i++)
+{
+  arr.push(randomcolor());
+}
+
+return arr;
+
+}
+
+function randomcolor() {
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+
+}
